87d8f8a7a22e569e7b135eacb901c25161176299
Trog19 <tsrogers8@msn.com>
2022-05-31 09:15:24 -0600

    Updated repository working out bug in SuperSerializer fields list.

--- supers/serializers.py
+++ supers/serializers.py
@@ -1,9 +1,13 @@
 from tarfile import SUPPORTED_TYPES
 from rest_framework import serializers
-from .models import Super
+
+from super_types import models
+from . models import Super
+from super_types.models import SuperType
 
 class SuperSerializer(serializers.ModelSerializer):
     class Meta:
         model = Super
         fields = ['id', 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'super_type', 'super_type_id']
+        super_type = models.SuperType(write_only=True)
         super_type_id = serializers.IntegerField(write_only=True)
